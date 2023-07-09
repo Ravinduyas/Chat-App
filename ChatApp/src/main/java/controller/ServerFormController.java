9a610992e00a8ef7145c94a4ffdb792a0de6e6d4
Ravinduyas <ravindu.yasanka21@gmail.com>
2023-07-09 23:08:39 +0530

    initial

---
+++ ChatApp/src/main/java/controller/ServerFormController.java
@@ -0,0 +1,67 @@
+package controller;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ServerFormController {
+    public VBox vBox;
+    public ScrollPane scrollPain;
+    public AnchorPane pane;
+
+
+    public void initialize(){
+
+    }
+
+
+
+    public static void receiveMessage(String msgFromClient){
+        HBox hBox = new HBox();
+        hBox.setAlignment(Pos.CENTER_LEFT);
+        hBox.setPadding(new Insets(5,5,5,10));
+
+        Text text = new Text(msgFromClient);
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setStyle("-fx-background-color: #abb8c3; -fx-font-weight: bold; -fx-background-radius: 20px");
+        textFlow.setPadding(new Insets(5,10,5,10));
+        text.setFill(Color.color(0,0,0));
+
+        hBox.getChildren().add(textFlow);
+
+    }
+
+    public void addButtonOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(pane.getScene().getWindow());
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"something went wrong. can't add client").show();
+        }
+        stage.setTitle("Group Chat");
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.show();
+    }
+
+}
